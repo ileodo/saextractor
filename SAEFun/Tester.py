4cc0604e0f839c2d0d3760ad6ffae96ffae65911
iLeoDo <iLeoDo@gmail.com>
2015-06-26 21:51:03 +0100

    begin extraction

--- SAEFun/Tester.py
+++ SAEFun/Tester.py
@@ -16,18 +16,18 @@ import DTreeLearner
 #     )
 
 
-for x in xrange(1,20):
-    param = config.dtree_param.copy()
-    param['min_samples_leaf']=x
-    print "===== x:%d =====" %x
-    DTreeLearner.learn_dtree(
-        config.path_root + "/data/dataset2/train.csv",
-        config.path_root + "/data/dtree",
-        param
-    )
+# for x in xrange(1,20):
+param = config.dtree_param.copy()
+# param['min_samples_leaf']=x
+# print "===== x:%d =====" %x
+DTreeLearner.learn_dtree(
+    config.path_root + "/data/dataset2/train.csv",
+    config.path_root + "/data/dtree",
+    param
+)
 
-    DTreeLearner.test_data(
-        config.path_root + "/data/dtree",
-        config.path_root + "/data/dataset2/test.csv",
-        config.path_root + "/data/result.csv",
-    )
+DTreeLearner.test_data(
+    config.path_root + "/data/dtree",
+    config.path_root + "/data/dataset2/test.csv",
+    config.path_root + "/data/result.csv",
+)
